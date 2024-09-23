4451e4647812a69690f7875e751b1c27c67e9333
Jeeva261 <js3316573@gmail.com>
2024-09-23 12:06:01 +0530

    upload

--- practise.py
+++ practise.py
@@ -8,21 +8,18 @@ from sklearn.metrics import r2_score,mean_squared_error
 # 1.Implement linear regression on a dataset and interpret the coefficients
 # 2.Evaluate the model using R^2 and MSE
 
+
 file_path=r'C:\Users\ADMIN\OneDrive\Desktop\AIML githup\Regression\exp.csv'
 df=pd.read_csv(file_path)
-print(df)
+print(df.head())
 
-x=df[["Salary"]]
-y=df["YearsExperience"]
-print(x)
-print(y)
+x=df[["YearsExperience"]]
+y=df["Salary"]
+print(x,y)
 
-
-X_train, X_test, Y_train, Y_test = train_test_split(x, y, test_size=0.2,random_state=0)
-print(X_test)
-print(X_train)
-print(Y_test)
-print(Y_train)
+X_train,X_test,Y_train,Y_test=train_test_split(x,y,test_size=0.2,random_state=0)
+print(X_test,Y_test)
+print(X_train,Y_train)
 
 model=LinearRegression()
 res=model.fit(X_train,Y_train)
@@ -31,24 +28,26 @@ print(res)
 y_pred=model.predict(X_test)
 print(y_pred)
 
-print(f'slope:{model.coef_[0]}')
-print(f'intercept:{model.intercept_}')
+slope=model.coef_
+print(f'slope:{slope}')
 
+intercept=model.intercept_
+print(f'intercept:{intercept}')
 
 r2=r2_score(Y_test,y_pred)
-print(f'r2:{r2}')
+print(f'r square:{r2}')
 
 mse=mean_squared_error(Y_test,y_pred)
-print(f'mse:{mse}')
+print(f'mean square error:{mse}')
 
-YearsExperience=5.5
-predicted_salary=model.predict([[YearsExperience]])
-print(predicted_salary[0])
-
-
-plt.scatter(X_train, Y_train, color='blue')
-plt.plot(X_train, model.predict(X_train), color='red')
-plt.title('YearsExperience vs Salary (Training set)')
-plt.xlabel('Years of Experience')
-plt.ylabel('Salary')
+plt.scatter(X_train,Y_train,color="blue",label="Traind data")
+plt.plot(X_train,model.predict(X_train),color="red",label="regression line")
+plt.title("YearsExperience vs Salary")
+plt.xlabel("YearsExperience")
+plt.ylabel("Salary")
+plt.legend()
 plt.show()
+
+yearsexperience=5.5
+new_predict=model.predict([[yearsexperience]])
+print(new_predict[0])
